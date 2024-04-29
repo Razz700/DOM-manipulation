0f7f38dd6b5c48c6c5f86ab5551d3c8fa627d94c
Razz700 <160166041+Razz700@users.noreply.github.com>
2024-04-29 21:39:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let msg=document.getElementById("msg");
+let warn=document.getElementById("warning");
+let count=0;
+document.getElementById("dec").addEventListener("click",()=>{
+if(count==1){
+    document.getElementById("clear").style.display="none";
+}
+if(count==0){
+warn.innerHTML=`Error: Cannot go below 0`;
+}
+else{
+    count--;
+msg.innerHTML=`Your Current Count is: ${count}`;}
+});
+document.getElementById("inc").addEventListener("click",()=>{
+    count++;
+    warn.innerHTML=``;
+msg.innerHTML=`Your Current Count is: ${count}`;
+document.getElementById("clear").style.display="block";
+});
+document.getElementById("clear").addEventListener("click",()=>{
+    count=0;
+    document.getElementById("clear").style.display="none";
+    msg.innerHTML=`Your Current Count is: ${count}`;
+});
